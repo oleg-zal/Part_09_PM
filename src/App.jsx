1a3192ca9a3cb1a8b48bb6d8f121f08de259f625
oleg-zal <75122504+oleg-zal@users.noreply.github.com>
2024-06-19 06:15:25 +0000

    Add event addProject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProject: undefined,
+    projects: {},
+  });
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectID: null,
+      };
+    });
+  }
+  let content;
+  if (projectsState.selectedProject === null) {
+    content = <NewProject />;
+  } else if (projectsState.selectedProject === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar />
-      <NoProjectSelected />
+      <ProjectSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
